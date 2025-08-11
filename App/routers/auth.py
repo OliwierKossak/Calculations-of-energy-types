5f88e490b9649decc8bf823f9751a337358caae7
Oliwier Kossak <oliwierkossak109@gmail.com>
2025-08-11 07:32:56 +0200

    add method create user

--- App/routers/auth.py
+++ App/routers/auth.py
@@ -1,13 +1,37 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from ..models import User
 from pydantic import BaseModel, Field
+from typing import Annotated
+from sqlalchemy.orm import Session
 from datetime import datetime
+from starlette import status
+from ..database import get_db
+
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth']
+)
+
+db_dependency = Annotated[Session, Depends(get_db)]
 
 
-
-class User(BaseModel):
+class UserRequestModel(BaseModel):
     user_id: int
     email: str
     password: str
     role: str
     creation_date: datetime
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, user_request: UserRequestModel):
+    create_user_model = User(
+        user_id=user_request.user_id,
+        email=user_request.email,
+        password=user_request.password,
+        role=user_request.role,
+        creation_date=user_request.creation_date
+    )
+    db.add(create_user_model)
+    db.commit()
+
