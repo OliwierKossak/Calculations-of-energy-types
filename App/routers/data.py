9fd660331fbc32bff661becd37263a6c744241c5
Oliwier Kossak <oliwierkossak109@gmail.com>
2025-08-14 07:55:40 +0200

    add check of columns

--- App/routers/data.py
+++ App/routers/data.py
@@ -4,7 +4,7 @@ from ..models import User
 from pydantic import BaseModel, Field
 from typing import Annotated, Optional
 from sqlalchemy.orm import Session
-from datetime import datetime
+import pandas as pd
 from starlette import status
 from ..database import get_db
 from .auth import get_current_user
@@ -26,6 +26,21 @@ async def save_excel_data_to_db(file: UploadFile,user: user_dependency, db: db_d
     if not file.filename.endswith(('.xlsx', '.xls')):
         raise HTTPException(status_code=400, detail='Format of file need to be .xlsx or .xls')
 
+    try:
+        df = pd.read_excel(file.file)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error during loading file: {e}")
 
+    df.columns = [col.strip() for col in df.columns]
+
+    column_pairs = []
+    for column_value in df.columns:
+        if column_value.startswith("value"):
+            suffix = column_value[len("value"):]
+            column_type = "type" + suffix
+            if column_type in df.columns:
+                column_pairs.append((column_value, column_type))
+
+    flattened_data_from_file = []
 
 
